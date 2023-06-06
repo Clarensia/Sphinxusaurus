fb42823009db9318836c1719f77f7ac1bd6831ed
Nicolas <nicolas.went@epita.fr>
2023-06-06 15:21:37 +0200

    [ModelParser] Finished getting the attributes

--- src/parser/ModelParser.py
+++ src/parser/ModelParser.py
@@ -1,4 +1,5 @@
 import ast
+from typing import List, Tuple
 from src.dataclasses.model.ModelAttribute import ModelAttribute
 from src.parser.FileParser import FileParser
 from src.dataclasses.model.Model import Model
@@ -7,6 +8,36 @@ from src.utils import get_short_description
 class ModelParser(FileParser):
     """Parse a file that is inside of the "models" folder
     """
+
+    def _get_attribute_nodes(self, body: List[ast.Expr | ast.AnnAssign]) -> Tuple[ast.AnnAssign, ast.Expr]:
+        """Inside of the class body, there is our definition of the attribute
+        and there is also the documentation.
+        
+        The goal of this function is to pair the definition and the description.
+        
+        A definition is for example:
+        blockchain: str
+        
+        A description is the docstring that is below it, for example:
+        '''The id of the blockchain
+        
+        Example: ethereum
+        '''
+
+        All description looks like in the example description above
+
+        :param body: The body that we get from the class node
+        :type body: List[ast.Expr  |  ast.AnnAssign]
+        :return: At first the definition node and as second the expression node that
+                 contains the description of the attribute
+        :rtype: Tuple[ast.AnnAssign, ast.Expr]
+        """
+        ret = []
+        for i in range(1, len(body), 2):
+            # We know that the first docstring is always the documentation of the class
+            # the attributes starts from the second one
+            ret.append((body[i], body[i + 1]))
+        return ret
 
     def parse_file(self, file_path: str) -> Model:
         tree = self.get_tree(file_path)
@@ -18,23 +49,19 @@ class ModelParser(FileParser):
             if class_docstring:
                 ret.short_description = get_short_description(class_docstring)
                 ret.long_description = class_docstring
-            for child_node in node.body:
-                if isinstance(child_node, ast.Assign) and isinstance(child_node.targets[0], ast.Name):
-                    attribute = ModelAttribute()
-                    attribute.name = child_node.name
-                    if isinstance(child_node.annotation, ast.Name):
-                        attribute.attribute_type = child_node.annotation.id
-                    elif isinstance(child_node.annotation, ast.Str):  # Handle str type for Python 3.7
-                        attribute.attribute_type = child_node.annotation.s
-                    else:
-                        raise Exception(f"Unknown annotation type: {type(child_node.annotation)}")
+            attributes_desc = self._get_attribute_nodes(node.body)
+            for definition, description in attributes_desc:
+                attribute = ModelAttribute()
+                attribute.name = definition.target.id
+                if isinstance(definition.annotation, ast.Name):
+                    attribute.attribute_type = definition.annotation.id
+                elif isinstance(definition.annotation, ast.Str):  # Handle str type for Python 3.7
+                    attribute.attribute_type = definition.annotation.s
+                else:
+                    raise Exception(f"Unknown annotation type: {type(definition.annotation)}")
 
-                    docstring = ast.get_docstring(child_node)
-                    if docstring:
-                        docstring_lines = docstring.split("\nExample:")
-                        attribute.description = docstring_lines[0]
-                        attribute.example = docstring_lines[1]
-
-                    ret.attributes.append(attribute)
+                doc_lines = description.value.value.split("\n\nExample: ")
+                attribute.attribute_description = doc_lines[0]
+                attribute.example = doc_lines[1]
 
         return ret
